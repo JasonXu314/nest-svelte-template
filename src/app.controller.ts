d3fb670fd89c8e76f509815477843c191e9718c2
Jason Xu <Xinwen31415@gmail.com>
2024-01-29 13:28:58 -0600

    oops

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,13 @@
-import { Controller, Get, Param, Render } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Page } from './utils/decorators/page.decorator';
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get('/')
-	@Page()
-	getHello(): void {}
-
-	@Get('/:param')
-	@Render('index')
-	param(@Param('param') param): PageProps<{ initial: number }> {
-		return { initial: param.length };
+	getHello(): string {
+		return this.appService.getHello();
 	}
-
-	@Get('/test/:name')
-	@Page()
-	testName(): void {}
-
-	@Get('/:name/hi')
-	@Page()
-	idHi(): void {}
 }
 
